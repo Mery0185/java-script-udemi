97c6034234acf244c1946c77ca827e78a2ba31d6
Masha <mery01161985@gmail.com>
2025-08-05 16:19:07 +0200

    first commit

---
+++ js/lesson_1.js
@@ -0,0 +1,56 @@
+"use strict";
+
+// a = 15;
+// console.log(a);
+
+// const obj = {
+//   a: 50,
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+// alert("Have a nice day my love!");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+// const answer = prompt("Вам есть 18?", " ");
+// console.log(answer);
+// console.log(typeof answer);
+
+// const answers = [];
+
+// answers[0] = prompt("Как Ваше имя?", "");
+// answers[1] = prompt("Как Ваша фамилия?", "");
+// answers[2] = prompt("Сколько Вам лет?", "");
+
+// document.write(answers);
+
+// const category = "toys";
+// console.log(
+//   "https://developer.mozilla.org/ru/docs/conflicting/Learn_web_development/Core/Styling_basics" +
+//     category
+// );
+// console.log(
+//   `https://developer.mozilla.org/ru/docs/conflicting/Learn_web_development/Core/Styling_basics${category}`
+// );
+
+// const user = "Ivan";
+// alert(`Hello, ${user}`);
+// console.log(`arr` + " - object");
+// console.log(`arr` + " - object");
+// console.log(4 + +"5");
+
+// let incr = 10,
+//   decr = 10;
+
+// incr++;
+// decr--;
+
+// console.log(incr);
+// console.log(decr);
+
+const isChecked = true,
+  isClose = false;
+
+console.log(!isChecked || isClose);
